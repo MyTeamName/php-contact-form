4a07a20eea5733a2d6999e7b0889f1dac6fca8c9
Jeff Puckett <jeff@jeffpuckett.com>
2017-12-23 21:29:32 +0000

    test add Contact factory

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Contact::class, function (Faker $faker) {
     return [
-        //
+        'full_name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'message' => $faker->text,
+        'phone' => $faker->phoneNumber,
     ];
 });
