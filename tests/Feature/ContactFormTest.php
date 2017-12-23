e393c3be2e57b9f0b1d0d9516b38f958750e6731
Jeff Puckett <jeff@jeffpuckett.com>
2017-12-23 21:31:56 +0000

    test add contact data provider

--- tests/Feature/ContactFormTest.php
+++ tests/Feature/ContactFormTest.php
@@ -5,20 +5,35 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Contact;
 
 class ContactFormTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_saves_contact_form_to_database()
+    public function getContactData() : array
     {
-        $data = [
-            'full_name' => 'Jeff Puckett',
-            'email' => 'jeff@jeffpuckett.com',
-            'message' => 'I love writing code to make the world a bit better.',
-            'phone' => '678 321 Puck', // (678) 321-7825
-        ];
+        $this->refreshApplication();
+        // https://stackoverflow.com/a/26774924/4233593
 
+        return [
+            'Jeff Puckett' => [[
+                'full_name' => 'Jeff Puckett',
+                'email' => 'jeff@jeffpuckett.com',
+                'message' => 'I love writing code to make things better.',
+                'phone' => '678 321 Puck', // (678) 321-7825
+            ]],
+            'factory' => [
+                factory(Contact::class)->make()->toArray(),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getContactData
+     */
+    public function test_saves_contact_form_to_database(array $data)
+    {
         $this->post('/contact', $data)->assertStatus(201);
 
         $this->assertDatabaseHas('contacts', $data);
